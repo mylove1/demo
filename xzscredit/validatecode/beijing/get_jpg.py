873286857b872c691b5a911346bf928279da7c8c
cooper <dbcooper@aliyun.com>
2016-09-23 18:46:16 +0800

    验证码

---
+++ xzscredit/validatecode/beijing/get_jpg.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+import requests
+url = "http://qyxy.baic.gov.cn/CheckCodeCaptcha?currentTimeMillis=1474626756666&num=7585"
+
+for x in range(30):
+    r = requests.get(url)
+    with open(r"C:\Users\cooper\Desktop\opp\beijing\a\%s.jpg"%x, 'wb') as f:
+        f.write(r.content)
+print r.status_code
